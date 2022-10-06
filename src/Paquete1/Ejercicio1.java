ae20f710e215a0ead18e8a8d06ecc2161c94ecad
Angi Casella <acasella@danielcastelao.org>
2022-10-06 22:27:11 +0200

    Cambio 1

--- src/Paquete1/Ejercicio1.java
+++ src/Paquete1/Ejercicio1.java
@@ -2,6 +2,6 @@ package Paquete1;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        System.out.println("Este es el ejercicio 1");
+        System.out.println("Modificación Ejercico 1");
     }
 }
